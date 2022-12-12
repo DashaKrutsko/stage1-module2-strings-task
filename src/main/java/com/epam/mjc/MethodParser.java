b793d8b3eb5503dbb42709828dfce627408805e5
DK <dasha.krutsko@gmail.com>
2022-12-12 13:48:22 +0300

    update

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,53 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class MethodParser {
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
-    public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+
+    public MethodSignature parseFunction(String signatureString) throws UnsupportedOperationException {
+        List<MethodSignature.Argument> argumentList = new ArrayList<>();
+        String[] methodAndArguments = signatureString.split("\\(");
+        String stringArguments = methodAndArguments[1].substring(0, methodAndArguments[1].length() - 1);
+        if (stringArguments.length() > 0) {
+            String[] arguments = stringArguments.split(", ");
+            for (String arg : arguments) {
+                String[] argString = arg.split(" ");
+                MethodSignature.Argument argument = new MethodSignature.Argument(argString[0], argString[1]);
+                argumentList.add(argument);
+            }
+        }
+        String methodDescription = methodAndArguments[0];
+        String[] descriptions = methodDescription.split(" ");
+        int lenght = descriptions.length;
+        String methodName = descriptions[lenght - 1];
+        MethodSignature methodSignature = new MethodSignature(methodName, argumentList);
+
+        if (lenght == 3) {
+            methodSignature.setReturnType(descriptions[1]);
+            methodSignature.setAccessModifier(descriptions[0]);
+        } else {
+            methodSignature.setReturnType(descriptions[0]);
+        }
+        return methodSignature;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,18 +1,32 @@
 package com.epam.mjc;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
+
+import static java.util.stream.Collectors.toList;
 
 public class StringSplitter {
 
     /**
      * Splits given string applying all delimeters to it. Keeps order of result substrings as in source string.
      *
-     * @param source source string
+     * @param source     source string
      * @param delimiters collection of delimiter strings
      * @return List of substrings
      */
+
+
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String delimit : delimiters) {
+            stringBuilder.append(delimit).append(",");
+        }
+        System.out.println(stringBuilder.toString());
+        StringTokenizer st = new StringTokenizer(source, stringBuilder.toString());
+        List<String> list = new ArrayList<>();
+        while (st.hasMoreElements()) {
+            list.add(st.nextToken());
+        }
+        return list;
+
     }
 }
